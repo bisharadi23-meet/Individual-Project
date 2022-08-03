7003a1bec043fecb8b5adda58dd45510b6c45ce5
bisharadi23-meet <109900012+bisharadi23-meet@users.noreply.github.com>
2022-08-03 10:34:59 +0300

    tem added

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -6,9 +6,26 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
+config= {
+  "apiKey": "AIzaSyCFZKBUpxUkdx3F1RLa1qcrfdZsmtzLwsQ",
+  "authDomain": "first-shop-5af57.firebaseapp.com",
+  "projectId": "first-shop-5af57",
+  "storageBucket": "first-shop-5af57.appspot.com",
+  "messagingSenderId": "866858112630",
+  "appId": "1:866858112630:web:0589397f8c6c781efbb157",
+  "measurementId": "G-E5ZDVMYLPY",
+  "databaseURL": "https://first-shop-5af57-default-rtdb.europe-west1.firebasedatabase.app/"}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
 
+@app.route('/', methods=['GET', 'POST'])
+def HOME():
+    return render_template("Home.html")
 
-
+@app.route('/store', methods=['GET', 'POST'])
+def LOGIN():
+    return render_template("Store.html")
 
 #Code goes above here
 
